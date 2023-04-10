f3fee0f9c752a02ddd6771f67ff00c72b3e403ab
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-04-10 14:09:03 +0700

    feat: add program dosen

---
+++ Dosen.cs
@@ -0,0 +1,40 @@
+﻿namespace CSDosen
+{
+    class Dosen
+    {
+        private int ID;
+        private string Nama;
+        private int Nik;
+        private string Gender;
+        private string Course;
+
+        public Dosen()
+        {
+            this.ID = 1;
+            this.Nama = "Dosen Pengampu";
+            this.Nik = 123456789;
+            this.Gender = "Male";
+            this.Course = "Pemrograma Berorientasi Objek";
+        }
+
+        public void attDosen(string name, int nik)
+        {
+            this.Nama = name;
+            this.Nik = nik;
+        }
+
+        public void addCourse(string name)
+        {
+            this.Course += $", {name}";
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"ID \t\t= {this.ID}");
+            Console.WriteLine($"Nama \t\t= {this.Nama}");
+            Console.WriteLine($"NIK \t\t= {this.Nik}");
+            Console.WriteLine($"Gender \t\t= {this.Gender}");
+            Console.WriteLine($"Course \t\t= {this.Course}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CSDosen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+            Console.WriteLine("Default Dosen Pengampu");
+            dosen.display();
+
+            Console.WriteLine("\nMasukkan nama dan NIK Dosen Baru: ");
+            dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+            dosen.display();
+
+            Console.WriteLine("\nMasukkan Mata Kuliah Baru");
+            dosen.addCourse(Console.ReadLine());
+            dosen.display();
+
+            Console.ReadLine();
+        }
+    }
+}
